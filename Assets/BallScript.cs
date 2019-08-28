c67dc654968e9b6f4183b9232e3e7cb75a184aff
nullmage <jsky.johnson@gmail.com>
2019-08-28 16:46:13 -0600

    Adds more info

---
+++ Assets/BallScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Force;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Random.insideUnitCircle * Force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        //GetComponent<Rigidbody>().AddForce(new Vector3(-50, 0, 0));
+        Debug.Log("Hit!");
+        if (coll.gameObject.name == "RightPaddle")
+        {
+            rb.AddForce(new Vector3(-1f, transform.position.y - coll.transform.position.y, 0)*Force);
+        }
+        if (coll.gameObject.name == "LeftPaddle")
+        {
+            rb.AddForce(new Vector3(1f, transform.position.y - coll.transform.position.y, 0) * Force);
+        }
+        if (coll.gameObject.name == "ScoreLeft")
+        {
+            GameManager.PlayerLeftScore++;
+            Reset();
+        }
+        if (coll.gameObject.name == "ScoreRight")
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        this.transform.position = Vector3.zero;
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+}
---
+++ Assets/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject rightPaddle;
+    public GameObject leftPaddle;
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation_r = Input.GetAxis("Right") * speed * Time.deltaTime;
+        float translation_l = Input.GetAxis("Left") * speed * Time.deltaTime;
+
+        rightPaddle.transform.position = rightPaddle.transform.position + new Vector3(0, translation_r, 0);
+        leftPaddle.transform.position = leftPaddle.transform.position + new Vector3(0, translation_l, 0);
+
+    }
+}
